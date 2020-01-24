17a9e34d7eb86dd05f5737827a08236b80ff632f
tim walker <tim.twalker@gmail.com>
2020-01-23 19:14:14 -0800

    UPdate number 6

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello Azure and Docker!</h1>
-        <p>Updated: 5</p>
+        <p>Updated 6 times</p>
       </header>
     </div>
   );
